ae94676ba936f8d0e8520da20404bdd8bb6080d3
seirra blake <sophietheopossum@yandex.ru>
2022-01-20 05:37:30 +0000

    fix comment indentation

--- src/main/java/org/libsdl/api/stdinc/SdlStdinc.java
+++ src/main/java/org/libsdl/api/stdinc/SdlStdinc.java
@@ -24,34 +24,34 @@ public final class SdlStdinc {
         return a | (b << 8) | (c << 16) | (d << 24);
     }
 
-//    public static native String SDL_getenv(String name);
+    //    public static native String SDL_getenv(String name);
 
-//    public static native int SDL_setenv(String name, String value, int overwrite);
+    //    public static native int SDL_setenv(String name, String value, int overwrite);
 
-//    // TODO: Test and then possibly enable
-//    public static native Pointer SDL_malloc(size_t size);
-//
-//    public static native Pointer SDL_calloc(int nmemb, size_t size);
-//
-//    public static native Pointer SDL_realloc(Pointer mem, size_t size);
+    //    // TODO: Test and then possibly enable
+    //    public static native Pointer SDL_malloc(size_t size);
+    //
+    //    public static native Pointer SDL_calloc(int nmemb, size_t size);
+    //
+    //    public static native Pointer SDL_realloc(Pointer mem, size_t size);
 
     public static native void SDL_free(Pointer mem);
 
-//    /**
-//     * Get the current set of SDL memory functions
-//     */
-//    public static native void SDL_GetMemoryFunctions(PointerByReference malloc_func,
-//                                                     PointerByReference calloc_func,
-//                                                     PointerByReference realloc_func,
-//                                                     PointerByReference free_func);
-//
-//    /**
-//     * Replace SDL's memory allocation functions with a custom set
-//     */
-//    public static native int SDL_SetMemoryFunctions(Pointer malloc_func,
-//                                                    Pointer calloc_func,
-//                                                    Pointer realloc_func,
-//                                                    Pointer free_func);
+    //    /**
+    //     * Get the current set of SDL memory functions
+    //     */
+    //    public static native void SDL_GetMemoryFunctions(PointerByReference malloc_func,
+    //                                                     PointerByReference calloc_func,
+    //                                                     PointerByReference realloc_func,
+    //                                                     PointerByReference free_func);
+    //
+    //    /**
+    //     * Replace SDL's memory allocation functions with a custom set
+    //     */
+    //    public static native int SDL_SetMemoryFunctions(Pointer malloc_func,
+    //                                                    Pointer calloc_func,
+    //                                                    Pointer realloc_func,
+    //                                                    Pointer free_func);
 
     /**
      * Get the number of outstanding (unfreed) allocations
