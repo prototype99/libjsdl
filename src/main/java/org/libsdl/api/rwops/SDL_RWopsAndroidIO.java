65d6e613d0d3e1a8a5c857df5c234e2dd7f4c285
seirra blake <sophietheopossum@yandex.ru>
2022-01-20 06:07:17 +0000

    don't use a single element array

--- src/main/java/org/libsdl/api/rwops/SDL_RWopsAndroidIO.java
+++ src/main/java/org/libsdl/api/rwops/SDL_RWopsAndroidIO.java
@@ -3,9 +3,7 @@ package org.libsdl.api.rwops;
 import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
 
-@Structure.FieldOrder({
-        "asset"
-})
+@Structure.FieldOrder("asset")
 public final class SDL_RWopsAndroidIO extends Structure {
 
     public Pointer asset;
